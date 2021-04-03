80bfedf6ce5a0a82976ed7ae19a6bb0fe7b346c8
emre gazel <emregazel@hotmail.com>
2021-04-03 16:12:19 +0300

    ilk gönderim

---
+++ chmontenegrocompany.meDemo/chmontenegrocompany.meDemo/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace chmontenegrocompany.meDemo.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            FormsAuthentication.SetAuthCookie(username, false);
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ chmontenegrocompany.meDemo/chmontenegrocompany.meDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chmontenegrocompany.meDemo.MvcApplication" Language="C#" %>
